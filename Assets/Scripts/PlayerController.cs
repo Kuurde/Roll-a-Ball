0eea3fb1916eafa31f601c314ae6d162c712cd4b
Kurt Taffein <kurt.taffein@gmail.com>
2017-10-14 14:26:51 +0200

    Adjusted game for mobile controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,8 +22,8 @@ public class PlayerController : MonoBehaviour
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
